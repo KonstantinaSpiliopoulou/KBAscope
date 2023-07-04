b48ce4d599a6f053db8266e380165fbed6521509
KonstantinaSpiliopoulou <74048762+KonstantinaSpiliopoulou@users.noreply.github.com>
2023-07-04 17:41:20 +0300

    Update view_results.R

--- R/view_results.R
+++ R/view_results.R
@@ -55,8 +55,8 @@ view_results <- function(x,y, pic=TRUE) {
     # loop to integrate the pics #
     for(i in 1:length(temp1)){ 
       a1 <- a1 + rphylopic::add_phylopic(rphylopic::get_phylopic(
-        dplyr::filter(taxonomic_groups_codes, 
-        Taxonomic.group.level==grp[i])$uui), x=temp2[i], y=temp1[i]+h.adjust, ysize=0.25)
+        base::unique(dplyr::filter(taxonomic_groups_codes, 
+        Taxonomic.group.level==grp[i])$uui)), x=temp2[i], y=temp1[i]+h.adjust, ysize=0.25)
     }
     rm(temp1)
     rm(temp2)
